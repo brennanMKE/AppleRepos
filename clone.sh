3f112012527d6926ad9111af5c114a2997a337db
Brennan Stehling <stehlib@amazon.com>
2022-03-25 18:25:50 -0700

    revises scripts to page through repos

--- clone.sh
+++ clone.sh
@@ -10,17 +10,16 @@ mkdir -p Repos
 cd Repos
 pwd
 
-for APPLE_REPO in ${APPLE_REPOS}; do 
+for APPLE_REPO in ${APPLE_REPOS}; do
     if [ ! -d "${APPLE_REPO}" ]; then
         git clone git@github.com:apple/$APPLE_REPO.git
     fi
 done
 
-for OSS_REPO in ${OSS_REPOS}; do 
+for OSS_REPO in ${OSS_REPOS}; do
     if [ ! -d "${OSS_REPO}" ]; then
         git clone git@github.com:opensource-apple/$OSS_REPO.git
     fi
 done
 
 popd
-
--- refresh.sh
+++ refresh.sh
@@ -3,13 +3,64 @@
 # Refreshes list of repos from GitHub
 
 JQ=$(which jq)
-
 if [ -z "${JQ}" ]; then
-    echo "Install JQ to continue: brew install jq"
-    return
+  echo "Install JQ to continue: brew install jq" && exit 1
 fi
 
-curl "https://api.github.com/orgs/apple/repos?per_page=100" | ${JQ} -r ".[].name" | \
-    grep -v "ml-" | grep -v "ccs-" | grep -v "FHIR" | grep -v "HomeKit" | \
-    grep -v "jdk" | grep -v "turicreate" \
-    | sort | uniq > Apple.txt
+if [ -z "${GITHUB_USERNAME}" ]; then
+  echo "Define GITHUB_USERNAME to run" && exit 1
+fi
+
+if [ -z "${GITHUB_API_READ_ACCESS_TOKEN}" ]; then
+  echo "Define GITHUB_API_READ_ACCESS_TOKEN to run" && exit 1
+fi
+
+ORG=apple
+PER_PAGE=100
+
+TEMP_FILE=$(mktemp -t Apple)
+PAGE=1
+DONE=false
+
+echo "" >${TEMP_FILE}
+
+fetch_page() {
+  local PAGE=$1
+
+  curl -u "${GITHUB_USERNAME}:${GITHUB_API_READ_ACCESS_TOKEN}" \
+    "https://api.github.com/orgs/${ORG}/repos?per_page=${PER_PAGE}&page=${PAGE}" |
+    ${JQ} -r ".[].name"
+}
+
+append_list() {
+  local LIST=$1
+
+  echo "${LIST}" |
+    grep -v "ml-" | grep -v "ccs-" | grep -v "FHIR" |
+    grep -v "HomeKit" |
+    grep -v "jdk" | grep -v "turicreate" >>${TEMP_FILE}
+}
+
+append_lists() {
+  local PAGE=1
+  local MAX_PAGE=100
+  local DONE=false
+
+  until [ $PAGE -gt $MAX_PAGE -o $DONE = true ]; do
+    echo "# Page ${PAGE}" >>${TEMP_FILE}
+    local LIST=$(fetch_page $PAGE)
+    local COUNT=$(echo "${LIST}" | wc -l | bc)
+    if [ $PER_PAGE -gt $COUNT ]; then
+      DONE=true
+    else
+      PAGE=$(expr $PAGE + 1)
+    fi
+
+    append_list "${LIST}"
+  done
+
+  cat ${TEMP_FILE} | grep -v "#" | grep -v -e '^$' | sort | uniq >Apple.txt
+  rm ${TEMP_FILE}
+}
+
+append_lists
